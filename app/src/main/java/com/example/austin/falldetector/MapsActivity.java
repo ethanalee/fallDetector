7178eb92492c62b3e465e184fa4eac58485e2992
stinface98 <austin.atmaja@gmail.com>
2018-05-26 03:26:12 -0400

    added location detection

--- app/src/main/java/com/example/austin/falldetector/MapsActivity.java
+++ app/src/main/java/com/example/austin/falldetector/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.austin.falldetector;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
