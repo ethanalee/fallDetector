03ec48fae224957e35289b8f833c61ac482a83d5
Ethan Lee <nathe.eel@gmail.com>
2018-05-26 12:27:29 -0400

    added user tracking

---
+++ app/src/main/java/com/example/austin/falldetector/trackedUsers.java
@@ -0,0 +1,20 @@
+package com.example.austin.falldetector;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class trackedUsers extends AppCompatActivity {
+    public DataManager dataManager = new DataManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracked_users);
+
+        SharedPreferences sharedPref = this.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String email = sharedPref.getString("USER_EMAIL", "");
+        dataManager.registerUser(email, "fakeRecord");
+    }
+}
