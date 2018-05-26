b4f7fe60e161613ea0853f23abb9266437fc4f84
stinface98 <austin.atmaja@gmail.com>
2018-05-26 15:05:51 -0400

    wip

--- app/src/main/java/com/example/austin/falldetector/trackedUsers.java
+++ app/src/main/java/com/example/austin/falldetector/trackedUsers.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,6 +24,10 @@ public class trackedUsers extends AppCompatActivity {
 
     String userId;
     EditText email;
+    EditText lastFell;
+
+    TextView trackedUser;
+    String trackedUserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,8 @@ public class trackedUsers extends AppCompatActivity {
 
         Button register_button = findViewById(R.id.register_button);
         email = (EditText) findViewById(R.id.email_edittext);
+        lastFell = (TextView) findViewById(R.id.last_fell);
+        trackedUser = (TextView)findViewById(R.id.tracked_user);
 
         register_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -41,20 +48,53 @@ public class trackedUsers extends AppCompatActivity {
             }
         });
 
-        // Read from the database
+        // Read user
         myRef = database.getReference(userId);
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+                String trackedEmail = dataSnapshot.getValue(String.class);
+                trackedUserEmail = trackedEmail;
+                if(trackedEmail != null){
+                    trackedUser.setText(trackedEmail);
+                    Log.d("New Tracked User", trackedEmail);
+                }
+            }
 
-                Boolean lol = true;
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("FireBaseError", "Failed to read value.", error.toException());
+            }
+        });
+
+        // Read falls from database
+        myRef = database.getReference("falls");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
 
                 for (DataSnapshot snapshotChild: dataSnapshot.getChildren()) {
                     String value = snapshotChild.getValue(String.class);
                     if( value != null ){
+                        String[] failRecord = value.split("/");
+                        String email = failRecord[0];
+                        Double lat = Double.parseDouble(failRecord[1]);
+                        Double longitude = Double.parseDouble(failRecord[2]);
+                        if(email == trackedUserEmail){
+                            String key = dataSnapshot.getKey();
+                            String[] keyArray = key.split("-");
+                            Long timeStampMillis = Long.parseLong(keyArray[1]);
+                            if(System.currentTimeMillis() - timeStampMillis < 3600){
+                                lastFell.setText(timeStampMillis.toString());
+                            }
 
+                        }
+                        //addMarker(lat, longitude);
                     }
                 }
                 Log.d("Firebase Updated", "yay");
